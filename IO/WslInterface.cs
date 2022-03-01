e29f8f5bbc03d14f83b31882843febc5a720d554
Roy de Jong <roy@softwarepunt.nl>
2022-03-01 18:14:10 +0100

    wip: WSL interface

---
+++ IO/WslInterface.cs
@@ -0,0 +1,91 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Text;
+
+namespace WSLHostsUpdater.IO;
+
+public class WslInterface
+{
+    protected ILogger _logger;
+
+    public WslInterface(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<bool> TryGetIsWslRunning()
+    {
+        var result = await Execute("-l -v");
+
+        if (result == null)
+            return false;
+
+        var lines = result.Split(Environment.NewLine);
+
+        foreach (var line in lines)
+        {
+            if (line.StartsWith('*'))
+            {
+                // Default instance, this is what we're looking for
+                return line.Contains("Running"); 
+            }
+        }
+
+        _logger.LogWarning("are you kitten me");
+        return false;
+    }
+    
+    public async Task<IPAddress?> TryGetAddress()
+    {
+        var result = await Execute("hostname -I");
+
+        if (result is null)
+            return null;
+        
+        if (IPAddress.TryParse(result.Trim(), out var ipAddress))
+            return ipAddress;
+
+        return null;
+    }
+
+    internal async Task<string?> Execute(string args)
+    {
+        var psi = new ProcessStartInfo("wsl", args);
+        psi.RedirectStandardOutput = true;
+        psi.StandardOutputEncoding = Encoding.UTF8;
+        psi.RedirectStandardError = true;
+        psi.StandardErrorEncoding = Encoding.UTF8;
+        psi.WindowStyle = ProcessWindowStyle.Hidden;
+        psi.CreateNoWindow = true;
+
+        try
+        {
+            var process = new Process();
+            process.StartInfo = psi;
+            process.Start();
+            
+            _logger.LogDebug("[WSL] Execute: wsl {Args}", args);
+
+            await process.WaitForExitAsync();
+            
+            if (process.StandardError.Peek() > -1)
+            {
+                var stderr = await process.StandardError.ReadToEndAsync();
+                _logger.LogWarning("[WSL] stderr: {Text}", stderr);
+            }
+
+            if (process.StandardOutput.Peek() > -1)
+            {
+                var stdOut = await process.StandardOutput.ReadToEndAsync();
+                stdOut = stdOut.Replace("\0", string.Empty); // remove null bytes (why are they here??)
+                return stdOut;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("[WSL] Execute failed");
+        }
+        
+        return null;
+    }
+}
--- Worker.cs
+++ Worker.cs
@@ -1,20 +1,49 @@
+using WSLHostsUpdater.IO;
+
 namespace WSLHostsUpdater;
 
 public class Worker : BackgroundService
 {
+    private const int CheckInterval = 30000;
+    
     private readonly ILogger<Worker> _logger;
+    private readonly WslInterface _wsl;
 
     public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
+        _wsl = new WslInterface(logger);
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+            await Refresh();
+            await Task.Delay(CheckInterval, stoppingToken);
         }
+    }
+
+    private async Task<bool> Refresh()
+    {
+        var wslIsRunning = await _wsl.TryGetIsWslRunning();
+
+        if (!wslIsRunning)
+        {
+            _logger.LogWarning("WSL default instance is not running, skipping refresh");
+            return false;
+        }
+
+        var ipAddress = await _wsl.TryGetAddress();
+
+        if (ipAddress is null)
+        {
+            _logger.LogWarning("Failed to get IP address from WSL default instance, skipping refresh");
+            return false;
+        }
+        
+        _logger.LogInformation("WSL instance is running ({Address}), refreshing...", ipAddress);
+        // TODO
+        return true;
     }
 }
